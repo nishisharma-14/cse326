b34ec14077fcd72e08da8a542d077e2e0ab4b858
nishisharma-14 <184118285+nishisharma-14@users.noreply.github.com>
2025-07-06 13:00:02 +0530

    Set basename in BrowserRouter and updated .gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cse326">
       <App />
     </BrowserRouter>
   </React.StrictMode>
