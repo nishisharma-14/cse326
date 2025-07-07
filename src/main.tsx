962494a781583674785801f78951cbd4b0a034d7
param20h <paramjitsingh.23@lpu.in>
2025-07-07 18:34:00 +0530

    router

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/cse326">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
